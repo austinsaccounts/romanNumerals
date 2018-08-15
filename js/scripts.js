f1d2ca773d4024217e4265d832cdcb1dbcb4a251
skwerl_e@yahoo.com and Ben Kiggen <skwerl_e@yahoo.com@>
2018-08-15 15:09:06 -0700

    q works, conson doesnt

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,71 @@
-$(document).ready(function(){
-  $("form#input").submit(function(event){
-    var originalText = $("#original-text").val();
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    var originalText = $("#original-text").val().toLowerCase();
     var originalTextArray = originalText.split(" ");
     var latinWord;
 
-    originalTextArray.forEach(function(word){
+
+
+    originalTextArray.forEach(function(word) {
+
+      var brokenWord = word.split("");
       //vowel condition
-      if(word.match(/\b[a,e,i,o,u]/)){
+      if (word.match(/\b[aeiou]/)) {
         $("#result").text(word + "way");
       }
       //consonant conditions
-        else {
+      else {
+
+
+        brokenWord.forEach(function(letter) {
+
+          if (letter.match(/[aeiou]/)) {
+            
+            console.log("vowel");
+          }
+          else if (letter.match(/qu/)) {
+
+          var  letter = brokenWord.shift();
+          letter = letter + brokenWord.shift();
+          brokenWord = brokenWord + letter;
+          console.log(brokenWord + "ay");
+        } else if (letter.match(/[^a,e,i,o,u,q]/)){
+          var letter = brokenWord.shift();
+
+          console.log(brokenWord + letter +  "ay");
+        }
 
 
 
 
-
-
-
-
-
-
-      }
+        });
+      };
     });
-    event.preventDefault();
   });
 });
+
+
+
+
+// var letter = brokenWord.shift();
+// letter = letter + brokenWord.shift();
+// alert(brokenWord.join("") + letter + "ay" );
+
+
+//logics for making word here
+// find elements
+
+//turns word into array by number
+
+
+
+
+//endingLetters += first letter,-- shift()removes that letter from broken word.
+
+//
+// alert(brokenWord.join("") + letter + "ay" );
+// // expected output: Array [2, 3]
+//
+// console.log(firstElement);
+// // expected output: 1
